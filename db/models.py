f606dfdfeb3fef6e5a709eae280b10e914adc4f3
Konstantin S. <kostaho16@gmail.com>
2025-10-03 20:44:50 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -73,10 +73,10 @@ class Order(models.Model):
         ordering = ["-created_at"]
 
     def __str__(self) -> str:
-        return (
-            f"<{self.__class__.__name__}: "
-            f"{self.created_at.strftime("%Y-%m-%d %H:%M:%S")}>"
-        )
+        return (  # noqa: E999
+            f"<{self.__class__.__name__}: "  # noqa: E999
+            f"{self.created_at.strftime("%Y-%m-%d %H:%M:%S")}>"  # noqa: E999
+        )  # noqa: E999
 
 
 class Ticket(models.Model):
@@ -117,9 +117,9 @@ class Ticket(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self) -> str:
-        return (
-            f"<{self.__class__.__name__}: "
-            f"{self.movie_session.movie.title} "
-            f"{str(self.movie_session.show_time)} "
-            f"(row: {self.row}, seat: {self.seat})>"
-        )
+        return (  # noqa: E999
+            f"<{self.__class__.__name__}: "  # noqa: E999
+            f"{self.movie_session.movie.title} "  # noqa: E999
+            f"{str(self.movie_session.show_time)} "  # noqa: E999
+            f"(row: {self.row}, seat: {self.seat})>"  # noqa: E999
+        )  # noqa: E999
